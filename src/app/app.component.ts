e7a43ec59df1af313b9f92592e9ffaf57945cb35
denukaNirmalee <nirdima@gmail.com>
2018-08-10 15:52:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng5-http-get-post';
+  
+  name: string = '';
+  age: number;
+  found:boolean;
+
+  constructor (private httpClient: HttpClient){  }
+
+  onNameKeyUp(event : any){
+    this.name = event.target.value;
+    this.found = false;
+  }
+
+  getProfile(){
+    this.httpClient.get(`https://my-json-server.typicode.com/denukaNirmalee/json-faker-directory/profiles/?name=${this.name}`)
+    .subscribe(
+      (data: any[])=> {
+        //console.log(data)
+        if(data.length){
+          this.age = data[0].age;
+          this.found = true;
+        }
+      }
+    )
+  }
+
+  postProfile(){
+    this.httpClient.post(`https://my-json-server.typicode.com/denukaNirmalee/json-faker-directory/profiles/`,
+    {
+      name:'marko',
+      age: 41
+    } )
+    .subscribe(
+      (data: any)=> {
+        console.log(data)
+        
+      }
+    )
+  }
 }
